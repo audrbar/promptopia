6875c19192716c68a1f978398d1fb8d455604c4e
audrbar <audrbar@gmail.com>
2024-12-26 19:26:53 +0200

    voiceflow

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,6 +10,18 @@ export const metadata = {
 const Rootlayout = ({ children }) => {
     return (
         <html lang="en">
+            <Head>
+                <Script type="text/javascript"
+                    (function(d, t) {var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+                v.onload = function() {
+                    window.voiceflow.chat.load({
+                        verify: { projectID: '65f6f0b6558bed48960bee3c' },
+                        url: 'https://general-runtime.voiceflow.com',
+                        versionID: 'production'
+                    });}
+                        v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);})(document, 'script');
+                />
+            </Head>
             <body>
                 <Provider>
                     <div className='main'>
