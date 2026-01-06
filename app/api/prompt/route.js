04e457652562a7707842d82eb9b4cc0425ca40b3
audrbar <audrbar@gmail.com>
2026-01-06 21:24:38 +0200

    Prompt API route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,19 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export const GET = async (request) => {
     try {
         await connectToDB()
         const prompts = await Prompt.find({}).populate('creator')
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), {
+            status: 200,
+            headers: {
+                'Cache-Control': 'no-store, max-age=0',
+            }
+        })
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
